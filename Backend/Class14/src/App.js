9340c64083e8e67859ede9b0ce39adc371e8dd41
Sujal Panchal <Sujalpanchal249@gmail.com>
2026-02-23 20:21:56 +0530

    post details and imagekit

--- Backend/Class14/src/App.js
+++ Backend/Class14/src/App.js
@@ -4,6 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieparser());
 const authRouter = require("./routes/user.routes");
+const postRouter = require("./routes/post.routes");
 app.use("/api/auth/", authRouter);
+app.use("/api/posts/", postRouter);
 
 module.exports = app;
